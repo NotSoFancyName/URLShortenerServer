1c86861fa34f3e2b25a943dd7256ef1004837cd1
Volodymyr Pankin <volopank@gmail.com>
2020-08-25 12:09:59 +0300

    Remove redundant operation

--- shortenerserver.go
+++ shortenerserver.go
@@ -5,12 +5,9 @@ import (
 	"net/http"
 
 	"github.com/NotSoFancyName/URLShortenerServer/handlers"
-	"github.com/NotSoFancyName/URLShortenerServer/shortener"
-	"github.com/NotSoFancyName/URLShortenerServer/persistance"
 )
 
 func main() {
-	shortener.SetCounter(persistance.GetCounter())
 	http.HandleFunc("/", handlers.DefaultHandler)
 	http.HandleFunc(handlers.ActionName, handlers.ShortenedURLHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
